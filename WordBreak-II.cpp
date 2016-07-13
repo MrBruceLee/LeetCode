223927169a4505416bacc090857f13e14e7d8f27
MrBruceLee <brucellz@hotmail.com>
2016-07-13 16:56:17 -0400

    Word Break II - Time Limit Exceeded

---
+++ WordBreak-II.cpp
@@ -0,0 +1,37 @@
+/*
+ *  DFS
+ */
+
+
+/*
+Time Limit Exceeded
+ 
+"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+ */
+
+class Solution {
+public:
+    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+        vector<string> results;
+        
+        resolveString(s, wordDict, "", results);
+        
+        return results;
+    }
+    
+private:
+    void resolveString(string s, unordered_set<string>& wordDict, string sentence, vector<string>& results) {
+        if(s.length() <= 0) {
+            sentence.pop_back();
+            results.push_back(sentence);
+        }
+        
+        for(int i = 1; i <= s.length(); i++) {
+            string subs = s.substr(0, i);
+            if(wordDict.find(subs) != wordDict.end()) {
+                resolveString(s.substr(i), wordDict, sentence + subs + " ", results);
+            }
+        }
+    }
+};
