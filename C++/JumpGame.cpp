0d29629a4411d2dac6b30aae6c0cc01998ca45f5
MrBruceLee <brucellz@hotmail.com>
2016-07-27 14:05:00 -0400

    jump game update

--- C++/JumpGame.cpp
+++ C++/JumpGame.cpp
@@ -1,19 +1,52 @@
+/*
+ *  Greedy
+ *  Runtime - O(N)  Space - O(1)
+ */
+
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int steps = nums[0];
+        int maxLen = 0;
         
-        for(int i = 1; i < nums.size(); i++){
-            steps -= 1;
-            
-            if(steps < 0){
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > maxLen) {
                 return false;
             }
             
-            // greedy
-            steps = max(steps, nums[i]);
+            maxLen = max(maxLen, i + nums[i]);
         }
         
         return true;
+    }
+};
+
+
+
+
+/*
+ *  Dynamic Programming
+ *  Runtime - O(N^2)    Space - O(N)
+ */
+
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        vector<bool> can(nums.size(), false);
+        can[0] = true;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i - 1; j >= 0; j--) { // optimize for the test case: [1,1,1,1,1,......,1,1,1,1,1]
+            // for(int j = 0; j < i; j++) {
+                
+                if(can[j] == true && j + nums[j] >= i) {
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return can.back();
     }
 };
