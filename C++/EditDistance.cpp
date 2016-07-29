0209e66b9206859b86edbeddfd69eed165168897
MrBruceLee <brucellz@hotmail.com>
2016-07-29 01:33:54 -0400

    format

--- C++/EditDistance.cpp
+++ C++/EditDistance.cpp
@@ -16,18 +16,21 @@ public:
         
         vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
         
-        for(int i = 0; i <= len1; i++)
-        dp[i][0] = i;
-        for(int j = 0; j <= len2; j++)
-        dp[0][j] = j;
+        for(int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0; j <= len2; j++) {
+            dp[0][j] = j;
+        }
         
         for(int i = 1; i <= len1; i++){
             for(int j = 1; j <= len2; j++){
                 
-                if(word1[i-1] == word2[j-1])
-                dp[i][j] = dp[i-1][j-1];
-                else
-                dp[i][j] = min( dp[i-1][j-1] + 1, min( dp[i-1][j] + 1, dp[i][j-1] + 1) );
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min( dp[i-1][j-1] + 1, min( dp[i-1][j] + 1, dp[i][j-1] + 1) );
+                }
             }
         }
         
