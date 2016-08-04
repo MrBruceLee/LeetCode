7dbef1923944bdbcc4c384a4c300daf2d51700b1
MrBruceLee <brucellz@hotmail.com>
2016-08-04 01:22:15 -0400

    reverse linked list 2 -- code update

--- C++/ReverseLinkedList-II.cpp
+++ C++/ReverseLinkedList-II.cpp
@@ -1,3 +1,10 @@
+/*
+ *  1. split into three lists
+ *  2. reverse the second list
+ *  3. combine into one list
+ */
+
+
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
@@ -6,24 +13,25 @@ public:
         head = dummy;
         
         ListNode* list1end = NULL;
+        ListNode* list2start = NULL;
         ListNode* list2end = NULL;
-        while(n >= 0) {
-            if(m == 1) {
+        ListNode* list3start = NULL;
+        
+        for(int i = 0; i <= n; i++) {
+            if(i == m - 1) {
                 list1end = head;
             }
-            if(n == 0) {
+            if(i == n) {
                 list2end = head;
             }
             head = head->next;
-            m--;
-            n--;
         }
         
-        ListNode* list2start = list1end->next;
-        ListNode* list3start = list2end->next;
+        list2start = list1end->next;
+        list3start = list2end->next;
+        
         list2end->next = NULL;
         list2end = list2start;
-        
         while(list2start != NULL) {
             ListNode* tmp = list2start->next;
             list2start->next = list1end->next;
@@ -35,4 +43,5 @@ public:
         
         return dummy->next;
     }
+    
 };
