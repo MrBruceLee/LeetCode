2d1ad05254586e0ded64e2cb1df7cbb9defacd94
MrBruceLee <brucellz@hotmail.com>
2016-08-15 21:02:19 -0400

    update find median from data stream

--- C++/FindMedianFromDataStream.cpp
+++ C++/FindMedianFromDataStream.cpp
@@ -7,42 +7,37 @@ public:
     
     // Adds a number into the data structure.
     void addNum(int num) {
-        if(max_heap.empty() == true || num <= max_heap.top()) {
-            max_heap.emplace(num);
+        if(maxHeap.empty() == true || num <= maxHeap.top()) {
+            maxHeap.push(num);
         } else {
-            min_heap.emplace(num);
+            minHeap.push(num);
         }
         
-        // the return type of size() is size_type, which is an unsigned variable
-        if((int)max_heap.size() - (int)min_heap.size() > 1) {
-            // if( max_heap.size() > min_heap.size() + 1) {
-            min_heap.emplace(max_heap.top());
-            max_heap.pop();
-            
-        } else if((int)min_heap.size() - (int)max_heap.size() > 0) {
-            // } else if(min_heap.size() > max_heap.size()) {
-            max_heap.emplace(min_heap.top());
-            min_heap.pop();
+        /* the return type of size() is size_type, which is an unsigned variable */
+        if(maxHeap.size() - 1 > minHeap.size()) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if(minHeap.size() > maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
     }
     
     // Returns the median of current data stream
     double findMedian() {
         double median = 0;
-        if(min_heap.size() == max_heap.size()) {
-            median = (double)(min_heap.top() + max_heap.top()) / 2.0;
+        if(maxHeap.size() == minHeap.size()) {
+            median = (double)(maxHeap.top() + minHeap.top()) / 2.0;
         } else {
-            median = (double)max_heap.top();
+            median = (double)maxHeap.top();
         }
-        
         return median;
     }
     
 private:
-    priority_queue<int, vector<int>, greater<int>> min_heap; // right side --> larger number
-    priority_queue<int, vector<int>, less<int>> max_heap; // left side  --> smaller number
+    priority_queue<int, vector<int>, less<int>> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
 };
-
 
 // Your MedianFinder object will be instantiated and called as such:
 // MedianFinder mf;
